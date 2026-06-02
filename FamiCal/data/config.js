c3f866b4770eed371918909b932c2454a1bb8e5b
Kitagawa Manabu <106717589+ruhmk@users.noreply.github.com>
2026-06-02 22:31:08 +0900

    Update config.js

--- FamiCal/data/config.js
+++ FamiCal/data/config.js
@@ -1,4 +1,4 @@
 window.FAMILY_CALENDAR_CONFIG = {
-  googleSyncUrl: "https://script.google.com/a/macros/qua-vision.com/s/AKfycbzrwGa_70EEuMhqoIV3Ydp99roBKzRjJbHxmgx2tiR0XFPCfpwxc6BVLl6Uf8nBFR69/exec",
+  googleSyncUrl: "https://script.google.com/a/macros/qualiarts.jp/s/AKfycbxzHtFZBbUqeeuNPsNMMfl8oA86jRhfP8Zkf2J4jheW0guyP-bIZegKyoViEIp8Y4yq/exec",
   autoLoadGoogleEvents: true
 };
