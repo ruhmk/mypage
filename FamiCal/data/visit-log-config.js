d81f9f98ad5cfa11a0ba74bda4f518aa7bce4d2f
Kitagawa Manabu <106717589+ruhmk@users.noreply.github.com>
2026-07-17 13:54:23 +0900

    Create visit-log-config.js

---
+++ FamiCal/data/visit-log-config.js
@@ -0,0 +1,4 @@
+window.FAMILY_VISIT_LOG_CONFIG = {
+  visitLogUrl: "https://script.google.com/a/macros/qualiarts.jp/s/AKfycbzFail1Dm6ChJJuClP20SNGZOauer7GacLFenKbF80OpcF4KmmWTMpA0xeVxdd5wrs/exec
+"
+};
